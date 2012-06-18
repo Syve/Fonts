7cc087b53caa0fa53e26e1aeee16684d3aec34c2
Brian Rowe <briprowe@yahoo.com>
2012-06-18 19:43:21 -0400

    Define IFont protocol.

--- src/syve/fonts/core.clj
+++ src/syve/fonts/core.clj
@@ -1,6 +1,8 @@
 (ns syve.fonts.core)
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+(defprotocol IFont
+  (width [this str] "Get the rendered width of the given string.")
+  (height [this str] "Get the rendered height of the given string.")
+  (line-height [this] "Get the max height of any line drawn by this font.")
+  (render ^BufferedImage [this s] "Render the given string into a BufferedImage"))
+
